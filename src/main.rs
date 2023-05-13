c6a94acd7488085042b61629532ec14f68e2b047
Gints Polis <polis.gints@gmail.com>
2023-05-13 10:29:55 +0300

    salvo with sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use salvo::{__private::once_cell::sync::OnceCell, prelude::*};
+use anyhow::{Result};
+use sqlx::sqlite::SqlitePool;
+use std::env;
+
+static SQLITE: OnceCell<SqlitePool> = OnceCell::new();
+
+#[inline]
+pub fn get_sqlite() -> &'static SqlitePool {
+    unsafe { SQLITE.get_unchecked() }
+}
+
+#[handler]
+async fn hello_world(res: &mut Response) -> Result<()> {
+    let recs = sqlx::query(
+        r#"
+SELECT config_type
+FROM TT_CONFIG
+        "#
+    )
+    .fetch_all(get_sqlite())
+    .await?;
+
+    res.render("hello world!");
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    _ = &env::set_var("DATABASE_URL", "sqlite:./data/db.db");
+    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+    SQLITE.set(pool).unwrap();
+
+    let router = Router::new().get(hello_world);
+    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    Server::new(acceptor)
+        .serve(router)
+        .await;
+    Ok(())
 }
